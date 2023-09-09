c5b8d843430ca9b0ee6dbd16b76d9c9dfbd4e78c
Christerlin <98984063+Christerlin@users.noreply.github.com>
2023-09-09 00:23:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ import axios from 'axios'
 
 global.owner = [
   ['50943947490', 'Jo3 Bot', true],
-  ['50941786740' 'joe', true],
+  ['']
   [''] 
 ] //Number of owners
 
-global.mods = ['919074692450','919074692450'] 
-global.prems = ['919074692450', '919074692450', '919074692450']
+global.mods = ['50943947490','50943947490'] 
+global.prems = ['50943947490', '50943947490', '50943947490']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
